7e67b6a62b6558b74734d82e65c67ba7973ed93d
GrishilovaMarina <mgrishilova@mail.ru>
2021-11-14 15:07:10 +0300

    lesson 3 task 1

---
+++ task_1_3.py
@@ -0,0 +1,9 @@
+def div_0(num_1, num_2):
+    if num_2 == 0:
+        print ("На ноль не делится")
+    else:
+        return num_1 / num_2
+num_1 = int(input())
+num_2 = int(input())
+print(div_0(num_1, num_2))
+
