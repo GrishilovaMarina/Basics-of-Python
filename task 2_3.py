6ac92f37c337b6a2401d1011e323f1b3dcce4c78
GrishilovaMarina <mgrishilova@mail.ru>
2021-11-14 15:07:47 +0300

    lesson 3 task 2

---
+++ task 2_3.py
@@ -0,0 +1,12 @@
+def list(name, surname, year_of_birth, city, email, tel):
+    result = [name, surname, year_of_birth, city, email, tel]
+    print(result)
+
+name = input('name:')
+surname = input('surname:')
+year_of_birth = input('year_of_birth:')
+city = input('city:')
+email = input('email:')
+tel = input('tel:')
+list(name, surname, year_of_birth, city, email, tel)
+
